4d0f8e1611be8a5753ab1483ff83873c3305a980
Umar-Azam <umerazam256@gmail.com>
2023-05-13 14:17:16 -0400

    GPU integrate test

--- test.py
+++ test.py
@@ -2,6 +2,16 @@
 
 # %%
 
-print("Container test")
+import sympy
 
+
+# %%
+# Define the matrix
+A = sympy.Matrix([
+    [0, 1],
+    [-1, 0]
+])
+theta = sympy.Symbol('theta')
+# %%
+B = sympy.exp(theta * A).simplify()
 # %%
