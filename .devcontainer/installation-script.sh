7c645d4dd127c801e4755aff66ee5e17e280fb57
Umar-Azam <umerazam256@gmail.com>
2024-01-29 05:13:30 +0000

    fixed miniconda installation in devcontainer

--- .devcontainer/installation-script.sh
+++ .devcontainer/installation-script.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-cd ~/miniconda3/bin/conda init bash
+USER=$(whoami)
+mkdir -p /home/$USER/miniconda3
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O /home/$USER/miniconda3/miniconda.sh
+bash /home/$USER/miniconda3/miniconda.sh -b -u -p /home/$USER/miniconda3
+rm -rf /home/$USER/miniconda3/miniconda.sh
+/home/$USER/miniconda3/bin/conda init bash
 echo "Build completed"
