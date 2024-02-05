9fad853302a47eedcc1c1419d35184afaaa1b265
Umar-Azam <92691687+Umar-Azam@users.noreply.github.com>
2024-02-05 16:13:05 +0000

    Changed Dockerfile and installation script to set up miniconda

--- .devcontainer/installation-script.sh
+++ .devcontainer/installation-script.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-cd ~/miniconda3/bin/conda init bash
+# Setting up miniconda on system
+USER=$(whoami)
+mkdir -p /home/$USER/miniconda3
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O /home/$USER/miniconda3/miniconda.sh
+bash /home/$USER/miniconda3/miniconda.sh -b -u -p /home/$USER/miniconda3
+rm -rf /home/$USER/miniconda3/miniconda.sh
+/home/$USER/miniconda3/bin/conda init bash
+
+
 echo "Build completed"
